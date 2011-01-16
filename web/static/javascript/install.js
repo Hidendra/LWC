eaebe0c67b894beb959b669e3b4da15cf3d1f27d
Tyler <hidendra@griefcraft.com>
2011-01-16 14:18:24 -0400

    Web panel static files

---
+++ web/static/javascript/install.js
@@ -0,0 +1,20 @@
+/* Modify the ajax callback to our own */
+form_ajax_callback = install_step;
+
+/* Hook in our install step to put the initial value */
+Event.observe(window, 'load', function() {
+	install_step();
+});
+
+/* Modify the progress bar if we're installing */
+function install_step()
+{
+	var curr_step = $('step').getValue();
+	
+	if(curr_step > 0)
+	{	
+		curr_step--;
+		
+		set_progress(curr_step * 25);
+	}
+}
