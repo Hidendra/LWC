6bb37c51d6050cfa34991938f55f47bc273ec38e
Tyler Blair <hidendra@griefcraft.com>
2011-03-19 05:00:46 -0300

    push final changes

--- src/main/com/griefcraft/util/LocaleClassLoader.java
+++ src/main/com/griefcraft/util/LocaleClassLoader.java
@@ -26,7 +26,7 @@ public class LocaleClassLoader extends ClassLoader {
 	@Override
 	protected URL findResource(String name) {
 		File file = new File(Updater.DEST_LIBRARY_FOLDER + "locale/" + name);
-
+		
 		try {
 			return new URL("file:/" + file.getAbsolutePath());
 		} catch (MalformedURLException e) {
