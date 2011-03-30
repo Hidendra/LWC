79c4785a33e5c51d232003db771735bbf0fe2133
Tyler <hidendra@griefcraft.com>
2011-03-30 01:50:01 -0300

    2.11 , also, this commit is for the Recommended Build #600+

--- src/main/com/griefcraft/lwc/LWCInfo.java
+++ src/main/com/griefcraft/lwc/LWCInfo.java
@@ -40,7 +40,7 @@ public class LWCInfo {
 	/**
 	 * LWC's version
 	 */
-	public static final double VERSION = 2.10;
+	public static final double VERSION = 2.11;
 
 	static {
 		FULL_VERSION = String.format("v%.2f", VERSION);
