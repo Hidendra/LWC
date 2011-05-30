960debdc1da8e66e72efe8caaac29ace8b5788c6
Tyler <hidendra@griefcraft.com>
2011-05-29 23:39:43 -0300

    3.00-rc1, and minor locale changes

--- src/main/com/griefcraft/lwc/LWCInfo.java
+++ src/main/com/griefcraft/lwc/LWCInfo.java
@@ -43,7 +43,7 @@ public class LWCInfo {
 	public static final double VERSION = 3.00;
 
 	static {
-		FULL_VERSION = String.format("v%.2f-alpha5", VERSION);
+		FULL_VERSION = String.format("v%.2f-rc1", VERSION);
 	}
 
 }
