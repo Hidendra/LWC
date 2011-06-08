d317649742b72199255b4b09113caa1e71f0d22e
Tyler <hidendra@griefcraft.com>
2011-06-08 18:06:06 -0300

    MySQLPost301 was somehow deleted.. Says last night, so I must've been sleeping.
    I'd hate to know what happens to know if I was instead drunk. The whole branch removed and griefcraft.com broken?

---
+++ src/main/com/griefcraft/migration/MySQLPost301.java
@@ -0,0 +1,22 @@
+package com.griefcraft.migration;
+
+import com.griefcraft.lwc.LWC;
+import com.griefcraft.sql.PhysDB;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MySQLPost301 {
+
+    /**
+     * Check for a new table prefix, to auto rename the table :-)
+     *
+     * @param lwc
+     */
+    public static void checkDatabaseConversion(LWC lwc) {
+
+
+    }
+
+}
