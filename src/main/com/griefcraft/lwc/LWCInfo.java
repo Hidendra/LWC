2e9066ecfff183bee4520bcab626014fa1f08487
Tyler <hidendra@griefcraft.com>
2011-06-08 18:08:27 -0300

    MySQLPost301 really was supposed to be deleted -- whoops. Okay, I'm paying attention now dammit!
    3.05

--- src/main/com/griefcraft/lwc/LWCInfo.java
+++ src/main/com/griefcraft/lwc/LWCInfo.java
@@ -40,7 +40,7 @@ public class LWCInfo {
     /**
      * LWC's version
      */
-    public static final double VERSION = 3.04;
+    public static final double VERSION = 3.05;
 
     static {
         FULL_VERSION = String.format("v%.2f", VERSION);
