f6b1a89b5629dc9c1c569ff17d6751e78833d815
Tyler <hidendra@griefcraft.com>
2011-06-17 12:33:11 -0300

    Oh my, Russian & Spanish! People sure have been busy! Also added the unit tests, not much at the moment, just a mock server. It does not run a local server, but at the same time all of my plugins I test with loaded fine with this mock server (Permissions, LWC, WorldEdit/WorldGuard, etc)
    I just need to find a way to mock a small chunk of world, since LWC
    searches the world when you click for a valid protection.

---
+++ src/tests/main/com/griefcraft/LWCTestSuite.java
@@ -0,0 +1,63 @@
+/**
+ * This file is part of LWC (https://github.com/Hidendra/LWC)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.griefcraft;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.Plugin;
+import org.junit.Test;
+
+import com.griefcraft.bukkit.MockServer;
+import com.griefcraft.lwc.LWC;
+import com.griefcraft.lwc.LWCPlugin;
+
+import static org.junit.Assert.*;
+
+public class LWCTestSuite {
+
+	/**
+	 * mock Bukkit server
+	 */
+	private static MockServer server = null;
+	private static Logger logger = Logger.getLogger("TestSuite");
+	
+	public LWCTestSuite() {
+		if(LWCTestSuite.server == null) {
+			LWCTestSuite.server = new MockServer();
+		}
+	}
+	
+	@Test
+	public void CheckLWC() {
+		assertNotNull(getLWC());
+	}
+	
+	/**
+	 * @return the LWC object
+	 */
+	private LWC getLWC() {
+		Plugin plugin = server.getPluginManager().getPlugin("LWC");
+		
+		if(plugin == null) {
+			return null;
+		}
+		
+		return ((LWCPlugin) plugin).getLWC();
+	}
+	
+}
