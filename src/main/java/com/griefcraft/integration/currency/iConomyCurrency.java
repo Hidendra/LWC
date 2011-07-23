a50ce87bc74ad2df12859febb85f95b8a09514f0
Tyler Blair <hidendra@griefcraft.com>
2011-07-23 17:18:26 -0300

    Abstraction layer for Permissions & Currency. Backwards compatibility for builds under 1000 are now 100% supported.
    Permissions 2.7 / 3.x will be used in place of PermissionsBukkit until you remove Permissions 2.7 / 3.x.

---
+++ src/main/java/com/griefcraft/integration/currency/iConomyCurrency.java
@@ -0,0 +1,73 @@
+package com.griefcraft.integration.currency;
+
+import org.bukkit.entity.Player;
+
+import com.griefcraft.integration.ICurrency;
+import com.iConomy.iConomy;
+import com.iConomy.system.Account;
+import com.iConomy.system.Holdings;
+
+public class iConomyCurrency implements ICurrency {
+	
+	public boolean isActive() {
+		return true;
+	}
+	
+	public double getBalance(Player player) {
+		Account account = iConomy.getAccount(player.getName());
+		
+		if(account == null) {
+			return 0;
+		}
+		
+		return account.getHoldings().balance();
+	}
+
+	public boolean canAfford(Player player, double money) {
+		Account account = iConomy.getAccount(player.getName());
+		
+		if(account == null) {
+			return false;
+		}
+		
+		return account.getHoldings().hasEnough(money);
+	}
+
+	public double addMoney(Player player, double money) {
+		Account account = iConomy.getAccount(player.getName());
+		
+		if(account == null) {
+			return 0;
+		}
+		
+		Holdings holdings = account.getHoldings();
+		holdings.add(money);
+		
+		return holdings.balance();
+	}
+
+	public double removeMoney(Player player, double money) {
+		// we're removing money, so it should be positive
+		if(money < 0) {
+			money = -money;
+		}
+		
+		Account account = iConomy.getAccount(player.getName());
+		
+		if(account == null) {
+			return 0;
+		}
+		
+		Holdings holdings = account.getHoldings();
+		
+		// this SHOULD be a transaction, ensure they have enough
+		if(!holdings.hasEnough(money)) {
+			return holdings.balance();
+		}
+		
+		holdings.subtract(money);
+		
+		return holdings.balance();
+	}
+	
+}
---
+++ src/main/java/com/griefcraft/integration/permissions/NijiPermissions.java
@@ -0,0 +1,48 @@
+package com.griefcraft.integration.permissions;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import com.griefcraft.integration.IPermissions;
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+
+public class NijiPermissions implements IPermissions {
+
+	/**
+	 * The Permissions handler
+	 */
+	private PermissionHandler handler = null;
+	
+	public NijiPermissions() {
+		Plugin permissionsPlugin = Bukkit.getServer().getPluginManager().getPlugin("Permissions");
+		
+		if(permissionsPlugin == null) {
+			return;
+		}
+		
+		handler = ((Permissions) permissionsPlugin).getHandler();
+	}
+	
+	public boolean isActive() {
+		return handler != null;
+	}
+	
+	public boolean permission(Player player, String node) {
+		if(handler == null) {
+			return false;
+		}
+		
+		return handler.permission(player, node);
+	}
+
+	public String getGroup(Player player) {
+		if(handler == null) {
+			return null;
+		}
+		
+		return handler.getPrimaryGroup(player.getWorld().getName(), player.getName());
+	}
+
+}
