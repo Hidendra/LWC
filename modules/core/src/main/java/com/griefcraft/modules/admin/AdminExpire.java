b6ad55fcab4ec9b83b4b6e4d863e25d7bfc44e73
Tyler Blair <hidendra@griefcraft.com>
2011-08-07 20:54:43 -0300

    closes #49 -- /lwc admin purge -remove playernames added, and lwc.fastRemoveProtections() added

--- modules/core/src/main/java/com/griefcraft/modules/admin/AdminExpire.java
+++ modules/core/src/main/java/com/griefcraft/modules/admin/AdminExpire.java
@@ -65,171 +65,18 @@ public class AdminExpire extends JavaModule {
 
         int threshold = (int) ((System.currentTimeMillis() / 1000L) - time);
         int count = 0;
-        int completed = 0;
 
-        List<Integer> toRemove = new LinkedList<Integer>();
-        List<Block> removeBlocks = null;
-        int totalProtections = lwc.getPhysicalDatabase().getProtectionCount();
-
-        if (shouldRemoveBlocks) {
-            removeBlocks = new LinkedList<Block>();
-        }
-
-        sender.sendMessage("Loading protections via STREAM mode");
-
-        try {
-            Statement resultStatement = lwc.getPhysicalDatabase().getConnection().createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            resultStatement.setFetchSize(Integer.MIN_VALUE);
-
-            String prefix = lwc.getPhysicalDatabase().getPrefix();
-            ResultSet result = resultStatement.executeQuery("SELECT " + prefix + "protections.id AS protectionId, " + prefix + "protections.type AS protectionType, x, y, z, flags, blockId, world, owner, password, date, last_accessed FROM " + prefix + "protections WHERE last_accessed <= " + threshold + " AND last_accessed > 0");
-
-            while (result.next()) {
-                Protection protection = lwc.getPhysicalDatabase().resolveProtectionNoRights(result);
-                World world = protection.getBukkitWorld();
-
-                count++;
-
-                if (count % 100000 == 0 || count == totalProtections || count == 1) {
-                    sender.sendMessage(Colors.Red + count + " / " + totalProtections);
-                }
-
-                if (world == null) {
-                    continue;
-                }
-
-                // remove the protection
-                toRemove.add(protection.getId());
-
-                // remove the block ?
-                if(shouldRemoveBlocks) {
-                    removeBlocks.add(protection.getBlock());
-                }
-
-                completed ++;
-            }
-
-            // Close the streaming statement
-            result.close();
-            resultStatement.close();
-
-            // flush all of the queries
-            push(lwc, sender, toRemove);
-
-            if(shouldRemoveBlocks) {
-                removeBlocks(lwc, sender, removeBlocks);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            ;
-        }
+        // remove the protections
+        int completed = lwc.fastRemoveProtections(sender, "last_accessed <= " + threshold + " AND last_accessed >= 0", shouldRemoveBlocks);
 
         // reset the cache
         if (completed > 0) {
-            LWC.getInstance().getCaches().getProtections().clear();
             LWC.getInstance().getPhysicalDatabase().precache();
         }
 
         lwc.sendLocale(sender, "protection.admin.expire.removed", "count", count);
 
         return CANCEL;
-    }
-
-    /**
-     * Remove a list of blocks from the world
-     * 
-     * @param lwc
-     * @param sender
-     * @param blocks
-     */
-    private void removeBlocks(LWC lwc, CommandSender sender, List<Block> blocks) {
-        int count = 0;
-
-        for(Block block : blocks) {
-            if(block == null || !lwc.isProtectable(block)) {
-                continue;
-            }
-
-            // possibility of a double chest
-            if(block.getType() == Material.CHEST) {
-                Block doubleChest = lwc.findAdjacentBlock(block, Material.CHEST);
-
-                if(doubleChest != null) {
-                    removeInventory(doubleChest);
-                    doubleChest.setType(Material.AIR);
-                }
-            }
-
-            // remove the inventory from the block if it has one
-            removeInventory(block);
-
-            // and now remove the block
-            block.setType(Material.AIR);
-
-            count ++;
-        }
-
-        sender.sendMessage("Removed " + count + " blocks from the world");
-    }
-
-    /**
-     * Remove the inventory from a block
-     *
-     * @param block
-     */
-    private void removeInventory(Block block) {
-        if(block == null) {
-            return;
-        }
-
-        if(!(block.getState() instanceof ContainerBlock)) {
-            return;
-        }
-
-        ContainerBlock container = (ContainerBlock) block.getState();
-        container.getInventory().clear();
-    }
-
-    /**
-     * Push removal changes to the database
-     *
-     * @param toRemove
-     */
-    public void push(LWC lwc, CommandSender sender, List<Integer> toRemove) throws SQLException {
-        final StringBuilder builder = new StringBuilder();
-        final int total = toRemove.size();
-        int count = 0;
-
-        // iterate over the items to remove
-        Iterator<Integer> iter = toRemove.iterator();
-
-        // the database prefix
-        String prefix = lwc.getPhysicalDatabase().getPrefix();
-
-        // create the statement to use
-        Statement statement = lwc.getPhysicalDatabase().getConnection().createStatement();
-
-        while (iter.hasNext()) {
-            int protectionId = iter.next();
-
-            if (count % 100000 == 0) {
-                builder.append("DELETE FROM " + prefix + "protections WHERE id IN (" + protectionId);
-            } else {
-                builder.append("," + protectionId);
-            }
-
-            if (count % 100000 == 99999 || count == (total - 1)) {
-                builder.append(")");
-                statement.executeUpdate(builder.toString());
-                builder.setLength(0);
-
-                sender.sendMessage(Colors.Green + "REMOVED " + (count + 1) + " / " + total);
-            }
-
-            count++;
-        }
-
-        statement.close();
     }
 
 }
