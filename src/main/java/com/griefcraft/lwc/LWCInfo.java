9ec26c28662feac494a9135ad6c331393fbc7449
Tyler Blair <hidendra@griefcraft.com>
2011-08-16 19:21:00 -0300

    /lwc admin version will now use VERSION (BUILD), in preperation for #65

--- src/main/java/com/griefcraft/lwc/LWCInfo.java
+++ src/main/java/com/griefcraft/lwc/LWCInfo.java
@@ -60,6 +60,6 @@ public class LWCInfo {
     		e.printStackTrace();
     	}
     	
-    	FULL_VERSION = String.format("%.2f-%s", VERSION, LWCPlugin.class.getPackage().getImplementationVersion());
+    	FULL_VERSION = String.format("%.2f (%s)", VERSION, LWCPlugin.class.getPackage().getImplementationVersion());
     }
 }
