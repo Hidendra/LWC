7403ebeaded4a61d08bf47fe0fab5133d5957f16
Tyler Blair <hidendra@griefcraft.com>
2011-09-03 12:02:51 -0300

    Closes #83 -- add Essentials Econ support.

--- src/main/java/com/griefcraft/integration/currency/BOSECurrency.java
+++ src/main/java/com/griefcraft/integration/currency/BOSECurrency.java
@@ -1,3 +1,20 @@
+/**
+ * This file is part of LWC (https://github.com/Hidendra/LWC)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.griefcraft.integration.currency;
 
 import com.griefcraft.integration.ICurrency;
@@ -5,13 +22,6 @@ import cosine.boseconomy.BOSEconomy;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-/**
- * Created by IntelliJ IDEA.
- * User: hidendra
- * Date: 13/08/11
- * Time: 2:06 PM
- * To change this template use File | Settings | File Templates.
- */
 public class BOSECurrency implements ICurrency {
 
     /**
---
+++ src/main/java/com/griefcraft/integration/currency/EssentialsCurrency.java
@@ -0,0 +1,98 @@
+/**
+ * This file is part of LWC (https://github.com/Hidendra/LWC)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.griefcraft.integration.currency;
+
+import com.earth2me.essentials.Essentials;
+import com.earth2me.essentials.api.Economy;
+import com.earth2me.essentials.api.NoLoanPermittedException;
+import com.earth2me.essentials.api.UserDoesNotExistException;
+import com.griefcraft.integration.ICurrency;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+public class EssentialsCurrency implements ICurrency {
+
+    /**
+     * The Essentials plugin object
+     */
+    private Essentials essentials;
+
+    public EssentialsCurrency() {
+        Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("Essentials");
+
+        if(plugin == null) {
+            return;
+        }
+
+        essentials = (Essentials) plugin;
+    }
+
+    public boolean isActive() {
+        return false;
+    }
+
+    public String format(double money) {
+        return Economy.format(money);
+    }
+
+    public String getMoneyName() {
+        return essentials.getSettings().getCurrencySymbol();
+    }
+
+    public double getBalance(Player player) {
+        try {
+            return Economy.getMoney(player.getName());
+        } catch(UserDoesNotExistException e) {
+            return 0d;
+        }
+    }
+
+    public boolean canAfford(Player player, double money) {
+        try {
+            return Economy.hasEnough(player.getName(), money);
+        } catch(UserDoesNotExistException e) {
+            return false;
+        }
+    }
+
+    public double addMoney(Player player, double money) {
+        try {
+            Economy.add(player.getName(), money);
+        } catch(UserDoesNotExistException e) {
+            return 0;
+        } catch(NoLoanPermittedException e) {
+            return 0;
+        }
+
+        return getBalance(player);
+    }
+
+    public double removeMoney(Player player, double money) {
+        try {
+            Economy.subtract(player.getName(), money);
+        } catch(UserDoesNotExistException e) {
+            return 0;
+        } catch(NoLoanPermittedException e) {
+            return 0;
+        }
+
+        return getBalance(player);
+    }
+
+}
