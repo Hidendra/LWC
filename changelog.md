e7d759101e5cdc52d1bb2e57ae3bd8448d4bb8c2
Tyler Blair <hidendra@griefcraft.com>
2011-09-12 18:25:29 -0300

    4.0.0-alpha3. Scheduler is complete and needs some minor changes and localized into the localization files. A bunch of new commands attached to it - changelog.md has the current commands ..

--- changelog.md
+++ changelog.md
@@ -1,3 +1,12 @@
+### 4.00-alpha3
+* Job scheduler. At the moment, it only supports Cleanup, but will support more in the future. It allows you to manually run specific jobs or automatically run them at a given time (they can be provided by outside sources, too.)
+* `/lwc schedule create JOBNAME TYPE` - Creates a job, for example: `/lwc schedule create weekly cleanup`
+* `/lwc schedule run JOBNAME` - Manually run a job, for example: `/lwc schedule run weekly`
+* `/lwc schedule check JOBNAME` - Look up when the job will next run, if it's to be automatically ran. It will tell you how long until the job runs.
+* `/lwc schedule autorun JOBNAME TIME` - Schedule a job to automatically run. For example, run the weekly job every week: `/lwc schedule autorun weekly 1 week` - you can combine times, e.g: `/lwc schedule autorun weekly 2 days 12 hours` = every 60 hours
+* `/lwc schedule list` - List all of the known jobs. YELLOW represents a job that is not automatically scheduled and must be automatically ran. GREEN represents a job that is automatically scheduled but is not a candidate to be ran yet. RED represents a job that is waiting to be ran.
+
+
 ### 4.00-alpha2
 * `/lwc admin report` has had a makeover and now also shows cache read/writes
 * Protection rights have been inlined with the main protections table.
--- modules/core/src/main/java/com/griefcraft/modules/schedule/ScheduleModule.java
+++ modules/core/src/main/java/com/griefcraft/modules/schedule/ScheduleModule.java
@@ -25,6 +25,10 @@ import com.griefcraft.scripting.event.LWCCommandEvent;
 import com.griefcraft.util.Colors;
 import com.griefcraft.util.StringUtils;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Date;
+import java.util.Set;
 
 public class ScheduleModule extends JavaModule {
 
@@ -34,7 +38,7 @@ public class ScheduleModule extends JavaModule {
             return;
         }
 
-        if (!event.hasFlag("schedule")) {
+        if (!event.hasFlag("schedule") && !event.hasFlag("tasks") && !event.hasFlag("task")) {
             return;
         }
 
@@ -45,13 +49,13 @@ public class ScheduleModule extends JavaModule {
         // We are using the command from here on out!
         event.setCancelled(true);
 
-        if (args.length < 2) {
-            lwc.sendSimpleUsage(sender, "/lwc schedule <action> <name> [...]");
+        if (args.length < 1) {
+            lwc.sendSimpleUsage(sender, "/lwc schedule <Action> [JobName] [...]");
             return;
         }
 
         String action = args[0].toLowerCase();
-        String name = args[1].toLowerCase();
+        String name = args.length > 1 ? args[1].toLowerCase() : "";
         String[] jobArgs = new String[0];
         String joinedArguments = ""; // equivilent to StringUtils.join(jobArgs);
 
@@ -67,6 +71,11 @@ public class ScheduleModule extends JavaModule {
 
         // Look for actions .. !
         if (action.equals("create")) {
+            if (args.length < 2) {
+                lwc.sendSimpleUsage(sender, "/lwc schedule <Action> [JobName] [...]");
+                return;
+            }
+
             if (job != null) {
                 sender.sendMessage(Colors.Red + "Job " + name + " already exists.");
                 return;
@@ -90,14 +99,26 @@ public class ScheduleModule extends JavaModule {
             job.setName(name);
             job.setType(handler.getType());
 
+            // add who created the Job
+            if (sender instanceof Player) {
+                job.getData().put("creator", ((Player) sender).getName());
+            } else {
+                job.getData().put("creator", "Console");
+            }
+
             // save it to the database
             job.save();
             sender.sendMessage(Colors.Green + "Created the job " + name + " successfully using the handler " + handler.getName());
         }
 
         else if (action.equals("run")) {
+            if (args.length < 2) {
+                lwc.sendSimpleUsage(sender, "/lwc schedule <Action> [JobName] [...]");
+                return;
+            }
+
             if (job == null) {
-                sender.sendMessage(Colors.Red + "Invalid job.");
+                sender.sendMessage(Colors.Red + "Invalid job specified.");
                 return;
             }
 
@@ -111,6 +132,117 @@ public class ScheduleModule extends JavaModule {
             long timeMillis = System.currentTimeMillis() - start;
 
             sender.sendMessage("Successfully ran the job (" + timeMillis + " ms)");
+        }
+
+        else if (action.equals("remove") || action.equals("delete")) {
+            if (args.length < 2) {
+                lwc.sendSimpleUsage(sender, "/lwc schedule <Action> [JobName] [...]");
+                return;
+            }
+
+            if (job == null) {
+                sender.sendMessage(Colors.Red + "Invalid job specified.");
+                return;
+            }
+
+            job.remove();
+            sender.sendMessage(Colors.Green + "Removed job successfully!");
+        }
+
+        else if (action.equals("list")) {
+            // force a reload of jobs
+            lwc.getJobManager().load();
+
+            // Get the jobs
+            Set<Job> jobs = lwc.getJobManager().getJobs();
+
+            if (jobs.size() == 0) {
+                sender.sendMessage(Colors.Red + "No jobs have yet been created.");
+                return;
+            }
+
+            // No pages as of yet (TODO)
+
+            String format = "%16s%16s%16s";
+            sender.sendMessage(Colors.Blue + String.format(format, "Name", "Handler", "Creator"));
+            sender.sendMessage(" ");
+
+            for(Job foundJob : jobs) {
+                IJobHandler handler = foundJob.getJobHandler();
+                String lineColour = "";
+
+                // check the time remaining, if applicable
+                if (foundJob.getNextRun() <= 0) {
+                    lineColour = Colors.Yellow;
+                } else {
+                    if (foundJob.getTimeRemaining() <= 0) {
+                        lineColour = Colors.Red;
+                    } else {
+                        lineColour = Colors.Green;
+                    }
+                }
+
+                sender.sendMessage(lineColour + String.format(format, foundJob.getName(), handler.getName(), foundJob.getData().get("creator")));
+            }
+        }
+
+        else if (action.equals("autorun")) {
+            if (args.length < 2) {
+                lwc.sendSimpleUsage(sender, "/lwc schedule <Action> [JobName] [...]");
+                return;
+            }
+
+            if (job == null) {
+                sender.sendMessage(Colors.Red + "Invalid job specified.");
+                return;
+            }
+
+            if (jobArgs.length == 0) {
+                lwc.sendSimpleUsage(sender, "/lwc schedule autoRun " + name + " Time (e.g 1 week)");
+                return;
+            }
+
+            // Attempt to parse the time ...
+            long parsedTimeSeconds = StringUtils.parseTime(joinedArguments);
+            long parsedTimeMillis = parsedTimeSeconds * 1000L;
+
+            if (parsedTimeSeconds == 0) {
+                sender.sendMessage(Colors.Red + "Invalid time: " + joinedArguments);
+                return;
+            }
+
+            // calculate the time to next run at
+            job.setNextRun(System.currentTimeMillis() + parsedTimeMillis);
+
+            // store the time interval for the next time it runs so it can recalculate
+            job.getData().put("autoRun", parsedTimeMillis);
+
+            // save it to the database and notify the player
+            job.save();
+            sender.sendMessage(Colors.Green + "The job will " + job.getName() + " run again in " + StringUtils.timeToString(parsedTimeSeconds));
+            sender.sendMessage(Colors.Green + "In server time, that is at " + new Date(job.getNextRun()).toString());
+        }
+
+        else if (action.equals("check")) {
+            if (job == null) {
+                sender.sendMessage(Colors.Red + "Invalid job specified.");
+                return;
+            }
+
+            long nextRun = job.getNextRun();
+
+            if (nextRun <= 0) {
+                sender.sendMessage(Colors.Red + "The job " + job.getName() + " is not set to automatically run.");
+            } else {
+                long timeRemaining = job.getTimeRemaining();
+
+                if (job.shouldRun()) {
+                    sender.sendMessage(Colors.Green + "The job " + job.getName() + " is waiting to be executed.");
+                    return;
+                }
+
+                sender.sendMessage(Colors.Green + "The job " + job.getName() + " will be executed in " + StringUtils.timeToString(timeRemaining / 1000L));
+            }
         }
     }
 
