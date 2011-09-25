c48ed34046463812f252217cf64a8d65df7b7221
Tyler Blair <hidendra@griefcraft.com>
2011-09-25 10:25:58 -0300

    Add /lwc history. Allows the viewing of all history items for a player (or everyone.) Dynamic arguments allow you to use the player name or page name anywhere:   /lwc history, /lwc history Hidendra 2, /lwc history 2 Hidendra, /lwc history 2, and /lwc history Hidendra   are all valid and will work! Later on, will work hand-in-hand with /lwc details <HistoryId>

--- modules/core/src/main/java/com/griefcraft/modules/history/HistoryModule.java
+++ modules/core/src/main/java/com/griefcraft/modules/history/HistoryModule.java
@@ -17,13 +17,153 @@
 
 package com.griefcraft.modules.history;
 
+import com.griefcraft.lwc.LWC;
 import com.griefcraft.model.History;
+import com.griefcraft.model.LWCPlayer;
 import com.griefcraft.model.Protection;
 import com.griefcraft.scripting.JavaModule;
+import com.griefcraft.scripting.event.LWCCommandEvent;
 import com.griefcraft.scripting.event.LWCProtectionRemovePostEvent;
+import com.griefcraft.util.Colors;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.List;
+
 public class HistoryModule extends JavaModule {
+
+    /**
+     * Amount of history items to show per page
+     */
+    public static final int ITEMS_PER_PAGE = 15;
+
+    @Override
+    public void onCommand(LWCCommandEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+
+        if (!event.hasFlag("h", "history")) {
+            return;
+        }
+
+        LWC lwc = event.getLWC();
+        CommandSender sender = event.getSender();
+        String[] args = event.getArgs();
+        event.setCancelled(true);
+
+        // Some vars we'll use more later on
+        boolean isWildcard = false;
+        int page = 1;
+        int historyCount = 0;
+        int pageCount = 0;
+
+        // If it's the console without arguments, lookup for every player
+        if (!(sender instanceof Player) && args.length == 0) {
+            isWildcard = true;
+        }
+
+        // The player name to try to parse
+        String playerName = null;
+
+        // check for page number
+        if (args.length > 0) {
+            try {
+                page = Integer.parseInt(args[0]);
+
+                if (args.length > 1) {
+                    playerName = args[1];
+                }
+            } catch (NumberFormatException e) {
+                playerName = args[0];
+
+                // look for page number
+                if (args.length > 1) {
+                    try {
+                        page = Integer.parseInt(args[1]);
+                    } catch (NumberFormatException ex) {
+                    }
+                }
+            }
+        }
+
+        // check for player string
+        if (playerName != null) {
+            // Are they an admin, to be able to do that?
+            if (!lwc.isAdmin(sender)) {
+                // Nope, go away!
+                lwc.sendLocale(sender, "protection.accessdenied");
+                return;
+            } else {
+                // Using a wildcard?
+                if (playerName.equals("*")) {
+                    isWildcard = true;
+                }
+            }
+        } else {
+            // if they're a player, look themselves up
+            if (sender instanceof Player) {
+                playerName = ((Player) sender).getName();
+            }
+        }
+
+        // Check if a player was STILL not found and just apply defaults if so
+        if (playerName == null) {
+            if (!(sender instanceof Player)) {
+                isWildcard = true;
+            } else {
+                playerName = ((Player) sender).getName();
+            }
+        }
+
+        // Get the first page
+        List<History> relatedHistory;
+
+        if (isWildcard) {
+            relatedHistory = lwc.getPhysicalDatabase().loadHistory((page - 1) * ITEMS_PER_PAGE, ITEMS_PER_PAGE);
+            historyCount = lwc.getPhysicalDatabase().getHistoryCount();
+        } else {
+            relatedHistory = lwc.getPhysicalDatabase().loadHistory(playerName, (page - 1) * ITEMS_PER_PAGE, ITEMS_PER_PAGE);
+            historyCount = lwc.getPhysicalDatabase().getHistoryCount(playerName);
+        }
+
+        // Calculate page count
+        if (historyCount > 0) {
+            pageCount = (int) Math.floor(historyCount / (page * ITEMS_PER_PAGE));
+
+            // compensate for what's left
+            while ((pageCount * ITEMS_PER_PAGE) < historyCount) {
+                pageCount ++;
+            }
+        }
+
+        // Were there any usable results?
+        if (relatedHistory.size() == 0) {
+            sender.sendMessage(Colors.Red + "No results found.");
+            return;
+        }
+        
+        // Is it a valid page? (this normally will NOT happen, with the previous statement in place!)
+        if (page > pageCount) {
+            sender.sendMessage(Colors.Red + "Page not found.");
+            return;
+        }
+
+        // Send it to them
+        String format = "%4s%12s%12s%12s";
+
+        // Header
+        sender.sendMessage(" ");
+        sender.sendMessage("To view extended details on a history item, use " + Colors.Yellow + "/lwc details <HistoryId>");
+        sender.sendMessage(" ");
+        sender.sendMessage(Colors.Yellow + String.format(format, "Id", "Player", "Type", "Status"));
+        sender.sendMessage(Colors.Yellow + "Showing " + relatedHistory.size() + " results on page: " + page + "/" + pageCount + " (" + historyCount + " total)");
+
+        // Send all that is found to them
+        for (History history : relatedHistory) {
+            sender.sendMessage(String.format(format, ("" + history.getId()), history.getPlayer(), history.getType(), history.getStatus()));
+        }
+    }
 
     @Override
     public void onPostRemoval(LWCProtectionRemovePostEvent event) {
