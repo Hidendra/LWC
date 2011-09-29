f4b8dc162200acd642d02f269a386c734c373e09
Tyler Blair <hidendra@griefcraft.com>
2011-09-28 22:51:17 -0300

    Support old & new Showcase API (sigh!)

--- modules/core/src/main/java/com/griefcraft/modules/flag/MagnetModule.java
+++ modules/core/src/main/java/com/griefcraft/modules/flag/MagnetModule.java
@@ -33,6 +33,7 @@ import com.griefcraft.model.Flag;
 import com.griefcraft.model.Protection;
 import com.griefcraft.scripting.JavaModule;
 import com.griefcraft.util.config.Configuration;
+import com.narrowtux.showcase.Showcase;
 import de.moritzschmale.Showcase.ShowcaseMain;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -192,8 +193,13 @@ public class MagnetModule extends JavaModule {
         boolean hasShowcase = Bukkit.getServer().getPluginManager().getPlugin("Showcase") != null;
 
         if (hasShowcase) {
-            // Now we can check for the showcase item
-            return ShowcaseMain.instance.getItemByDrop(item) != null;
+            try {
+                // Now we can check for the showcase item
+                return ShowcaseMain.instance.getItemByDrop(item) != null;
+            } catch (Throwable e) {
+                // It's the "Other" Showcase ...
+                return Showcase.instance.getItemByDrop(item) != null;
+            }
         }
 
         return false;
