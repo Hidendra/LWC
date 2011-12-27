76a9bc0037b96a6cee19a36c91ff5d43ebbdfa58
Tyler Blair <hidendra@griefcraft.com>
2011-12-26 21:22:56 -0400

    Closes #197: located and restored /lwc admin purgeregion ..

--- modules/core/src/main/java/com/griefcraft/modules/worldguard/WorldGuardModule.java
+++ modules/core/src/main/java/com/griefcraft/modules/worldguard/WorldGuardModule.java
@@ -29,15 +29,22 @@
 package com.griefcraft.modules.worldguard;
 
 import com.griefcraft.lwc.LWC;
+import com.griefcraft.model.Protection;
 import com.griefcraft.scripting.JavaModule;
+import com.griefcraft.scripting.event.LWCCommandEvent;
 import com.griefcraft.scripting.event.LWCProtectionRegisterEvent;
+import com.griefcraft.util.Colors;
 import com.griefcraft.util.config.Configuration;
+import com.sk89q.worldedit.BlockVector;
 import com.sk89q.worldedit.Vector;
 import com.sk89q.worldguard.bukkit.BukkitUtil;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.GlobalRegionManager;
 import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
@@ -63,6 +70,91 @@ public class WorldGuardModule extends JavaModule {
         if (plugin != null) {
             worldGuard = (WorldGuardPlugin) plugin;
         }
+    }
+
+    @Override
+    public void onCommand(LWCCommandEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+
+        if (!event.hasFlag("a", "admin")) {
+            return;
+        }
+
+        LWC lwc = event.getLWC();
+        CommandSender sender = event.getSender();
+        String[] args = event.getArgs();
+
+        if (!args[0].equals("purgeregion")) {
+            return;
+        }
+
+        // we have the right command
+        event.setCancelled(true);
+
+        // check for worldguard
+        if (worldGuard == null) {
+            sender.sendMessage(Colors.Red + "WorldGuard is not enabled.");
+            return;
+        }
+
+        if (args.length < 2) {
+            lwc.sendSimpleUsage(sender, "/lwc admin purgeregion <RegionName> [World]");
+            return;
+        }
+
+        if (!(sender instanceof Player) && args.length < 3) {
+            sender.sendMessage(Colors.Red + "You must specify the world name the region is in since you are not logged in as a player.");
+            return;
+        }
+
+        // the region
+        String regionName = args[1];
+
+        // the world the region is in
+        String worldName = args.length > 2 ? args[2] : "";
+
+        // get the world to use
+        World world = null;
+
+        if (!worldName.isEmpty()) {
+            world = lwc.getPlugin().getServer().getWorld(worldName);
+        } else {
+            world = ((Player) sender).getWorld();
+        }
+
+        // was the world found?
+        if (world == null) {
+            sender.sendMessage(Colors.Red + "Invalid world.");
+            return;
+        }
+
+        GlobalRegionManager regions = worldGuard.getGlobalRegionManager();
+
+        // get the region manager for the world
+        RegionManager regionManager = regions.get(world);
+
+        // try and get the region
+        ProtectedRegion region = regionManager.getRegion(regionName);
+
+        if (region == null) {
+            sender.sendMessage(Colors.Red + "Region not found. If you region is in a different world than you, please use: /lwc admin purgeregion " + regionName + " WorldName");
+            return;
+        }
+
+        BlockVector minimum = region.getMinimumPoint();
+        BlockVector maximum = region.getMaximumPoint();
+
+        // get all of the protections inside of the region
+        List<Protection> protections = lwc.getPhysicalDatabase().loadProtections(world.getName(), minimum.getBlockX(), maximum.getBlockX(), 0, 128, minimum.getBlockZ(), maximum.getBlockZ());
+
+        // remove all of them
+        for (Protection protection : protections) {
+            protection.remove();
+        }
+
+        sender.sendMessage(Colors.Green + "Removed " + protections.size() + " protections from the region " + regionName);
     }
 
     @Override
