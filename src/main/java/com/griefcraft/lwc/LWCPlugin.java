d7c6e6d5e11dca8e9aaf4c161d23b002fe345397
Tyler Blair <hidendra@griefcraft.com>
2012-01-05 16:06:51 -0400

    fix my settings to automatically remove unused imports

--- src/main/java/com/griefcraft/lwc/LWCPlugin.java
+++ src/main/java/com/griefcraft/lwc/LWCPlugin.java
@@ -54,7 +54,6 @@ import org.bukkit.event.server.ServerListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Locale;
