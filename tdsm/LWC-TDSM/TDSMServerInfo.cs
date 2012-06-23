19cd9905a79fb07e055f35281b1f03c9a4fafbaf
Tyler Blair <hidendra@griefcraft.com>
2012-06-23 13:50:19 -0300

    compile & build LWC-TDSM.dll (Terraria)! (please work...)

---
+++ tdsm/LWC-TDSM/TDSMServerInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria_Server;
+using com.griefcraft;
+
+namespace LWC_TDSM
+{
+    class TDSMServerInfo : ServerInfo
+    {
+        private LWCPlugin plugin;
+
+        public TDSMServerInfo(LWCPlugin plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public ServerMod getServerMod()
+        {
+            return ServerMod.TDSM;
+        }
+
+        public string getServerVersion()
+        {
+            return "TDSM #" + Statics.BUILD + " (Terraria " + Statics.VERSION_NUMBER + ")";
+        }
+
+        public string getLayerVersion()
+        {
+            return plugin.Version + " (C#)";
+        }
+    }
+}
---
+++ tdsm/LWC-TDSM/World/TDSMWorld.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria_Server;
+using com.griefcraft.world;
+
+namespace LWC_TDSM.World
+{
+    class TDSMWorld : com.griefcraft.world.World
+    {
+
+        /**
+         * The world's name
+         */
+        private string name;
+
+        public TDSMWorld(string name)
+        {
+            this.name = name;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public Block getBlockAt(int x, int y, int z)
+        {
+            return new TDSMBlock(this, Main.tile.At(x, y), x, y);
+        }
+    }
+}
