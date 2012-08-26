d60a939b1472a26b5c04204d1541fcf11797c6f3
Tyler Blair <hidendra@griefcraft.com>
2012-08-26 20:50:55 -0300

    0.00-SNAPSHOT -> 5.0.0-SNAPSHOT, shade metrics (not implemented), a start on translation implementation, should work fully

--- api/src/main/java/com/griefcraft/I18n.java
+++ api/src/main/java/com/griefcraft/I18n.java
@@ -1,19 +1,94 @@
 package com.griefcraft;
 
+import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.Enumeration;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 
 public class I18n {
 
     /**
+     * The private localization instance
+     */
+    private static I18n instance = new I18n();
+
+    /**
+     * The resource bundle that is to be used
+     */
+    private ResourceBundle bundle;
+
+    private I18n() { }
+
+    /**
      * Translate a message to the currently enabled message locale.
-     * TODO
      *
-     * @param message
-     * @param arguments
-     * @return
+     * @param message the message to translate
+     * @return The translated message
+     */
+    public static String _(String message) {
+        return instance.translate(message);
+    }
+
+    /**
+     * Translate a message to the currently enabled message locale.
+     *
+     * @param message the message to translate
+     * @param arguments the arguments to bind to any parameters in the message
+     * @return The translated message
      */
     public static String _(String message, Object... arguments) {
-        return MessageFormat.format(message, arguments);
+        return instance.translate(message, arguments);
+    }
+
+    /**
+     * Translate a message with the given arguments
+     *
+     * @param message the message to translate
+     * @return The translated message
+     */
+    private String translate(String message) {
+        // load the bundle if it hasn't been loaded yet
+        if (bundle == null || true) {
+            loadLanguageBundle();
+        }
+
+        System.out.println("bundle [" + message + "] => [" + bundle.getString(message) + "]");
+        return bundle.getString(message);
+    }
+
+    /**
+     * Translate a message with the given arguments
+     *
+     * @param message the message to translate
+     * @param arguments the arguments to bind to any parameters in the message
+     * @return The translated message
+     */
+    private String translate(String message, Object... arguments) {
+        // load the bundle if it hasn't been loaded yet
+        if (bundle == null) {
+            loadLanguageBundle();
+        }
+        System.out.println("test");
+
+        return MessageFormat.format(translate(message), arguments);
+    }
+
+    /**
+     * Load the language bundle
+     */
+    private void loadLanguageBundle() {
+        try {
+            bundle = new PropertyResourceBundle(getClass().getResourceAsStream("/Messages_en.properties"));
+
+            System.out.println("Loading keys");
+            Enumeration<String> keys = bundle.getKeys();
+            while (keys.hasMoreElements()) {
+                System.out.println("locale key loaded = \"" + keys.nextElement() + "\"");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
