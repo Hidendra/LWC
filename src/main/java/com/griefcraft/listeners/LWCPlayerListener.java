e3c0ec4c8b75e4817d63b9182a00b26efcf35efb
Tyler Blair <hidendra@griefcraft.com>
2012-11-23 19:36:48 -0400

    simplify some login that could be confusing

--- src/main/java/com/griefcraft/listeners/LWCPlayerListener.java
+++ src/main/java/com/griefcraft/listeners/LWCPlayerListener.java
@@ -223,7 +223,12 @@ public class LWCPlayerListener implements Listener {
                 // if they're left clicking a sign, don't cancel the event
                 // we don't cancel it so that the text doesn't disappear from the sign
                 // if it clashes with a block break event. silly Bukkit :-(
-                if (!((material == Material.SIGN_POST || material == Material.WALL_SIGN) && event.getAction() == Action.LEFT_CLICK_BLOCK)) {
+                if (material == Material.SIGN_POST || material == Material.WALL_SIGN) {
+                    if (event.getAction() != Action.LEFT_CLICK_BLOCK) {
+                        event.setCancelled(true);
+                        event.setUseInteractedBlock(org.bukkit.event.Event.Result.DENY);
+                    }
+                } else {
                     event.setCancelled(true);
                     event.setUseInteractedBlock(org.bukkit.event.Event.Result.DENY);
                 }
