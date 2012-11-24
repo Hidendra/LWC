e555831f4c4200dd2fe908c60cfdc3526af9500f
Tyler Blair <hidendra@griefcraft.com>
2012-11-24 00:37:09 -0400

    uninitialise dat int

--- src/main/java/com/griefcraft/util/Updater.java
+++ src/main/java/com/griefcraft/util/Updater.java
@@ -237,7 +237,7 @@ public class Updater {
 
                     // begin transferring
                     byte[] buffer = new byte[1024];
-                    int read = 0;
+                    int read;
 
                     while ((read = inputStream.read(buffer)) > 0) {
                         outputStream.write(buffer, 0, read);
