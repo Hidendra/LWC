da204bd6710274257fe456ffb2973f6e1d48f184
Tyler Blair <hidendra@griefcraft.com>
2013-01-03 01:38:59 -0400

    sqlite compatible schema

--- docs/database.sql
+++ docs/database.sql
@@ -1,40 +1,31 @@
--- ----------------------------
---  Table structure for protection_attributes
--- ----------------------------
 DROP TABLE IF EXISTS protection_attributes;
 CREATE TABLE protection_attributes (
-	protection_id INT(11) NOT NULL,
-	attribute_name VARCHAR(20) NOT NULL,
-	attribute_value VARCHAR(255) NOT NULL,
-	PRIMARY KEY (protection_id, attribute_name)
-) ENGINE=InnoDB;
+  protection_id int(11) NOT NULL,
+  attribute_name varchar(20) NOT NULL,
+  attribute_value varchar(255) NOT NULL,
+  PRIMARY KEY (protection_id,attribute_name)
+) ;
 
--- ----------------------------
---  Table structure for protection_roles
--- ----------------------------
 DROP TABLE IF EXISTS protection_roles;
 CREATE TABLE protection_roles (
-	protection_id INT(11) NOT NULL,
-	TYPE INT(11) NOT NULL,
-	NAME VARCHAR(30) NOT NULL,
-	role INT(11) NOT NULL,
-	INDEX (protection_id),
-	PRIMARY KEY (protection_id, TYPE, NAME)
-) ENGINE=InnoDB;
+  protection_id int(11) NOT NULL,
+  type int(11) NOT NULL,
+  name varchar(30) NOT NULL,
+  role int(11) NOT NULL,
+  PRIMARY KEY (protection_id,type,name)
+) ;
+CREATE INDEX protection_id ON protection_roles (protection_id);
 
--- ----------------------------
---  Table structure for protections
--- ----------------------------
 DROP TABLE IF EXISTS protections;
 CREATE TABLE protections (
-	id INT(11) NOT NULL AUTO_INCREMENT,
-	x INT(11) NOT NULL,
-	y INT(11) NOT NULL,
-	z INT(11) NOT NULL,
-	world VARCHAR(255) NOT NULL,
-	created INT(11) NOT NULL,
-	updated INT(11) NOT NULL,
-	accessed INT(11) NOT NULL,
-	UNIQUE POSITION (x, y, z, world),
-	PRIMARY KEY (id)
-) ENGINE=InnoDB;
+  id int(11) NOT NULL,
+  x int(11) NOT NULL,
+  y int(11) NOT NULL,
+  z int(11) NOT NULL,
+  world varchar(255) NOT NULL,
+  created int(11) NOT NULL,
+  updated int(11) NOT NULL,
+  accessed int(11) NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE UNIQUE INDEX position ON protections (x, y, z, world);
