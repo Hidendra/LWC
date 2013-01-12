4301f5f0da9667f126f96cfd15e5ef8842ef2338
Tyler Blair <hidendra@griefcraft.com>
2013-01-12 05:00:28 -0400

    update copyright heh

--- api/src/main/java/com/griefcraft/attribute/ProtectionAttributeFactory.java
+++ api/src/main/java/com/griefcraft/attribute/ProtectionAttributeFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2012, Tyler Blair
+ * Copyright (c) 2011-2013 Tyler Blair
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
--- api/src/main/java/com/griefcraft/util/config/ConfigUpdater.java
+++ api/src/main/java/com/griefcraft/util/config/ConfigUpdater.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2011 Tyler Blair. All rights reserved.
+ * Copyright (c) 2011-2013 Tyler Blair
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
