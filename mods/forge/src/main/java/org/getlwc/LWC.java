b8703d0463ac5beaae5f2fb059c8308499904523
Tyler Blair <hidendra@griefcraft.com>
2013-01-22 08:51:38 -0400

    copyright texts got real screwed up in Forge somehow.

--- mods/forge/src/main/java/org/getlwc/LWC.java
+++ mods/forge/src/main/java/org/getlwc/LWC.java
@@ -1,5 +1,5 @@
-package org.getlwc;/*
- * Copyright (c) 2011, 2012, Tyler Blair
+/*
+ * Copyright (c) 2011-2013 Tyler Blair
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
@@ -15,7 +15,7 @@ package org.getlwc;/*
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR,
  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
  * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
@@ -26,6 +26,8 @@ package org.getlwc;/*
  * authors and contributors and should not be interpreted as representing official policies,
  * either expressed or implied, of anybody else.
  */
+
+package org.getlwc;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
