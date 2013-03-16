fb01a24b3b92197778c89275ae52f60156fa6675
Tyler Blair <hidendra@griefcraft.com>
2013-03-16 02:18:40 -0300

    add Trapped Chests to core.yml by default the first time this version/build is ran. After that it can be safely removed if you do not desire it (you will also be told in the console this happened)

--- src/main/java/com/griefcraft/lwc/LWC.java
+++ src/main/java/com/griefcraft/lwc/LWC.java
@@ -1339,10 +1339,6 @@ public class LWC {
             names.add(material.toString().toLowerCase());
         }
 
-        if (material == Material.TRAPPED_CHEST) {
-            names.add("chest");
-        }
-
         // Add the wildcards last so it can be overriden
         names.add("*");
         names.add(material.getId() + ":*");
