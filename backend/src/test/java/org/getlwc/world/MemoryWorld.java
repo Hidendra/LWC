99aeb24afe45e5808c136bf5edad014dc08dd52d
Tyler Blair <hidendra@griefcraft.com>
2014-05-21 13:21:56 -0300

    Protection testing: use Location instead of a test-only WorldPoint class.

--- backend/src/test/java/org/getlwc/world/MemoryWorld.java
+++ backend/src/test/java/org/getlwc/world/MemoryWorld.java
@@ -1,6 +1,7 @@
 package org.getlwc.world;
 
 import org.getlwc.Block;
+import org.getlwc.Location;
 import org.getlwc.World;
 
 import java.util.HashMap;
@@ -11,7 +12,7 @@ public class MemoryWorld implements World {
     /**
      * The blocks in the world
      */
-    private Map<WorldPoint, Block> blocks = new HashMap<WorldPoint, Block>();
+    private Map<Location, Block> blocks = new HashMap<>();
 
     /**
      * {@inheritDoc}
@@ -24,7 +25,7 @@ public class MemoryWorld implements World {
      * {@inheritDoc}
      */
     public Block getBlockAt(int x, int y, int z) {
-        WorldPoint point = new WorldPoint(x, y, z);
+        Location point = new Location(this, x, y, z);
         Block block = blocks.get(point);
 
         if (block == null) {
@@ -33,37 +34,6 @@ public class MemoryWorld implements World {
         }
 
         return block;
-    }
-
-    private class WorldPoint {
-
-        int x, y, z;
-
-        public WorldPoint(int x, int y, int z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-
-        @Override
-        public int hashCode() {
-            int hash = 5;
-            hash += hash * 17 + x;
-            hash += hash * 17 + y;
-            hash += hash * 17 + z;
-            return hash;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (!(o instanceof WorldPoint)) {
-                return false;
-            }
-
-            WorldPoint op = (WorldPoint) o;
-            return x == op.x && y == op.y && z == op.z;
-        }
-
     }
 
 }
