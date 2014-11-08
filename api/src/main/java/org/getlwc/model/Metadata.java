462ab9c005251930770588cfac00280ad48870b5
Tyler Blair <hidendra@griefcraft.com>
2014-11-08 19:42:03 -0400

    Metadata: add equals & hashcode

--- api/src/main/java/org/getlwc/model/Metadata.java
+++ api/src/main/java/org/getlwc/model/Metadata.java
@@ -17,8 +17,32 @@ public final class Metadata {
         this.value = value;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Metadata metadata = (Metadata) o;
+
+        if (key != null ? !key.equals(metadata.key) : metadata.key != null) return false;
+        if (value != null ? !value.equals(metadata.value) : metadata.value != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = key != null ? key.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
+
     public String getKey() {
         return key;
+    }
+
+    public String getValue() {
+        return value;
     }
 
     public String asString() {
