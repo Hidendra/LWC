ccf22b3f5f15bcf27889120e38a6a01fdda23bd1
Tyler Blair <hidendra@griefcraft.com>
2014-11-10 22:49:45 -0400

    DescriptionComponent: hash -> description

--- backend/src/main/java/org/getlwc/content/component/DescriptionComponent.java
+++ backend/src/main/java/org/getlwc/content/component/DescriptionComponent.java
@@ -4,8 +4,8 @@ import org.getlwc.component.AbstractValueComponent;
 
 public class DescriptionComponent extends AbstractValueComponent<String> {
 
-    public DescriptionComponent(String hash) {
-        set(hash);
+    public DescriptionComponent(String description) {
+        set(description);
     }
 
 }
