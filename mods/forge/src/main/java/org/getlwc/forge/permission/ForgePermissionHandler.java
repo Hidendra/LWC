f7b1b01b5138dedd5886d89e8721132ca8b2096f
Tyler Blair <hidendra@griefcraft.com>
2014-12-03 02:27:38 -0400

    Forge: Fix checking if a player is an OP. Bit of a hack atm because of GameProfile not being accessible from my env.

--- mods/forge/src/main/java/org/getlwc/forge/permission/ForgePermissionHandler.java
+++ mods/forge/src/main/java/org/getlwc/forge/permission/ForgePermissionHandler.java
@@ -1,11 +1,17 @@
 package org.getlwc.forge.permission;
 
 import cpw.mods.fml.common.FMLCommonHandler;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.integrated.IntegratedServer;
+import net.minecraft.server.management.ServerConfigurationManager;
 import org.getlwc.entity.Player;
+import org.getlwc.forge.asm.AbstractMultiClassTransformer;
+import org.getlwc.forge.asm.CompilationType;
+import org.getlwc.forge.entity.ForgePlayer;
 import org.getlwc.permission.PermissionHandler;
 
+import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -50,9 +56,30 @@ public class ForgePermissionHandler implements PermissionHandler {
         if (server.isSinglePlayer()) {
             return server instanceof IntegratedServer && server.getServerOwner().equalsIgnoreCase(player.getName());
         } else {
-            // TODO ?
-            // return server.getConfigurationManager().getOps().contains(player.getName().toLowerCase());
-            return false;
+            ForgePlayer forgePlayer = (ForgePlayer) player;
+
+            // accessing the method directly currently throws a compile error because
+            // GameProfile cannot be resolved.
+            try {
+                Method isPlayerOpped = null; // (func_152596_g)
+
+                for (Method method : ServerConfigurationManager.class.getDeclaredMethods()) {
+                    if (method.getName().equals("func_152596_g")) {
+                        isPlayerOpped = method;
+                        break;
+                    }
+                }
+
+                Method getGameProfile = EntityPlayer.class.getDeclaredMethod(AbstractMultiClassTransformer.getMethodName("EntityPlayer", "getGameProfile", CompilationType.SRG));
+
+                // retrieve the player's profile
+                Object gameProfile = getGameProfile.invoke(forgePlayer.getHandle());
+
+                return (boolean) isPlayerOpped.invoke(server.getConfigurationManager(), gameProfile);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
         }
     }
 
