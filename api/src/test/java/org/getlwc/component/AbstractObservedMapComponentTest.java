fbef3f0ab5c935fe6a27e926cad21a7c33b19257
Tyler Blair <hidendra@griefcraft.com>
2014-12-09 16:04:51 -0400

    fix my earlier blunder that I didn't properly revert

--- api/src/test/java/org/getlwc/component/AbstractObservedMapComponentTest.java
+++ api/src/test/java/org/getlwc/component/AbstractObservedMapComponentTest.java
@@ -26,20 +26,6 @@
  * authors and contributors and should not be interpreted as representing official policies,
  * either expressed or implied, of anybody else.
  */
-/**
- * This copy of Woodstox XML processor is licensed under the
- * Apache (Software) License, version 2.0 ("the License").
- * See the License for details about distribution rights, and the
- * specific rights regarding derivate works.
- *
- * You may obtain a copy of the License at:
- *
- * http://www.apache.org/licenses/
- *
- * A copy is also included in the downloadable source code package
- * containing Woodstox, in file "ASL2.0", under the same directory
- * as this file.
- */
 package org.getlwc.component;
 
 import org.junit.Before;
